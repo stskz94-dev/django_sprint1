7044107f3a51331f2d8eca082fb4e524ce538752
Sergey Tsepilov <stskz94@gmail.com>
2024-03-06 23:37:27 +0500

    views

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 
 def about(request):
+    """О проекте"""
     template = 'pages/about.html'
     return render(request, template)
 
 
 def rules(request):
+    """Наши правила"""
     template = 'pages/rules.html'
     return render(request, template)
